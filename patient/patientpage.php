554f0b4df9a6af1feab2d7fe740a41a7e8771d07
Lisa-Marxmillian <131626240+Lisa-Marxmillian@users.noreply.github.com>
2023-10-01 20:54:21 +0300

    commit

--- patient/patientpage.php
+++ patient/patientpage.php
@@ -60,7 +60,7 @@ session_start();
       <h2>Profile Information</h2>
       <?php
       
-      require_once("dbconnect.php");
+      require_once("../dbconnect.php");
 
       $username = $_SESSION['username']; 
 
@@ -92,28 +92,6 @@ session_start();
     </section>
   </main>
 
-  <footer>
-    <div class="footer-column">
-      <div class="contact-info">
-        <h2>Contact Us</h2>
-        <p>123 Kilimani, Nairobi</p>
-        <p>Phone: (123) 456-7890</p>
-        <p>Email: info@pharmacare.com</p>
-      </div>
-      <div class="footer-links">
-        <a href="termsandconditions.html">Terms and Conditions</a>
-        <a href="Events.html">Events</a>
-        <a href="privacypolicy.html">Privacy Policy</a>
-      </div>
-    </div>
-    <div class="footer-column">
-      <div class="offers">
-        <h2>Special Offers</h2>
-        <p>Subscribe to our newsletter and get exclusive discounts.</p>
-        <a href="#" class="cta-button">Subscribe</a>
-      </div>
-      <p>&copy; 2023 Drug Dispensing Website. All rights reserved.</p>
-    </div>
-  </footer>
+  <?php include "footer.php" ?>
 </body>
 </html>
