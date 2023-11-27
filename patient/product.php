cb455fc22f5d6b624f53ffff5869482332ab16aa
Kinuthia-Claudia <claudiawkinuthia@gmail.com>
2023-11-27 09:57:42 +0300

    a

--- patient/product.php
+++ patient/product.php
@@ -7,25 +7,12 @@ session_start();
 
 <html lang="en">
 <body>
-  <header>
-    <div class="top-bar">
-      <div class="logo">
-        <img src="../graphics/0.png" alt="Logo" >
-      </div>
-      <div class="patient-info">
-      Welcome, <?php echo $_SESSION['username']; ?>
-      </div>
-    </div>
-    <nav>
-      <ul>
-      <li><a href="patientpage.php">Dashboard</a></li> 
-        <li><a href="prescriptionpatientview.php">My Prescriptions</a></li> 
-        <li><a href="product.php">Medication</a></li> 
-        <li><a href="../PharmaCare Homepage/contact.html">Contact Us</a></li>
-        <li><a href="../logout.php">Logout</a></li> 
-        <li>
-          <form class="search-form" action="product.php" method="post"> 
-            <input type="text" name="search_query" placeholder="Search..."> 
+   
+
+  <main>
+     
+        <form class="search-form" action="product.php" method="post">
+            <input type="text" name="search_query" placeholder="Search for a specific drug...">
             <button type="submit">Search</button>
         </form>
         <form action="" method="post">
